3f4381af8e8555410c91d993aad024f3dc47c85a
Czibike <czibike03@gmail.com>
2024-05-17 23:13:04 +0200

    sytling the categories pages and made the sidebar templete

---
+++ templates/RenderSidebar.js
@@ -0,0 +1,48 @@
+document.querySelector(".sideBar").innerHTML = `
+<details>
+    <summary>Kategóriák</summary>
+        <ol>
+            <li><a href="./categoties/hajápolás.html">hajápolás</a></li>
+            <li><a href="./categoties/bőrápolás.html">bőrápolás</a></li>
+            <li><a href="./categoties/smink.html">smink</a></li>
+            <li><a href="./categoties/egyéb.html">egyéb</a></li>
+        </ol>
+</details>
+<details>
+    <summary>Szolgáltatások</summary>
+        <ol>
+            <li><a href="./fodrász.html">Fodrászat</a></li>
+            <li><a href="./kozmetika.html">Kozmetika</a></li>
+          </ol>
+</details>
+<details>
+    <summary>Információk</summary>
+        <ol>
+            <li><a href="#">Rólunk</a></li>
+            <li><a href="#">Kedvezmények</a></li>
+        </ol>
+</details>
+<details>
+    <summary>Ügyfélszolgálat</summary>
+        <ol>
+            <li><a href="#">Szállítás</a></li>
+            <li><a href="#">Visszaküldés es reklamáció</a></li>
+            <li><a href="#">Webáruház értékelése</a></li>
+            <li><a href="ászf.html">ÁSZF</a></li>
+            <li><a href="#">Adatkezelési tájékoztató</a></li>
+        </ol>
+</details>
+<details>
+    <summary>Kapcsolatok</summary>
+        <p>Cím: 7400 Kaposvár horváth utca 28.</p>
+        <p>telefonszám:
+            <a href="#">+36 30/500 8548</a>
+        </p>
+        <p>E-mail:
+            <a href="#">PAIZSBOGLARKA15&ensp;@GMAIL.COM</a>
+        </p>
+        <p>E-mail:
+            <a href="#">BOGYSZEPESZET&ensp;@GMAIL.COM</a>
+        </p>
+</details>
+`;
