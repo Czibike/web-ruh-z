8445be872b8d4f9a91847452a592863e8284f9c3
Czibike <czibike03@gmail.com>
2024-05-20 20:45:39 +0200

    made the short and grid button

--- templates/RenderHairProducts.js
+++ templates/RenderHairProducts.js
@@ -8,18 +8,20 @@ window.onload = () => {
     html += `
       <div class="card">
         <img src=".${i.image}" alt="" />
-        <div class="textarea">
-          <h4 class="product-name js-productName">${i.name}</h4>
-          <h4 class="product-price">${i.price}</h4>
-        </div>
-        <div class="buttons">
-          <button class="cart">
-            <span>kosár</span>
-            <img src="../images/icons/add_shopping_cart.png" alt="cart" />
-          </button>
-          <button class="favorite">
-            <img src="../images/icons/favorite.png" alt="favorite" />
-          </button>
+        <div class="txt-and-btn">
+          <div class="textarea">
+            <h4 class="product-name js-productName">${i.name}</h4>
+            <h4 class="product-price">${i.price}</h4>
+          </div>
+          <div class="buttons">
+            <button class="cart">
+              <span>kosár</span>
+              <img src="../images/icons/add_shopping_cart.png" alt="cart" />
+            </button>
+            <button class="favorite">
+              <img src="../images/icons/favorite.png" alt="favorite" />
+            </button>
+          </div>
         </div>
       </div>`;
   }
