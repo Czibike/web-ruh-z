c34402fee19fdc316bbe50104dd45752d96a6486
Czibike <czibike03@gmail.com>
2024-07-13 21:38:20 +0200

    wrote table javascript

---
+++ javascript/fodraszTable.js
@@ -0,0 +1,18 @@
+const btns = document.querySelectorAll(".table-btn");
+const tables = document.querySelectorAll(".js-table");
+
+for (const btn of btns) {
+  btn.addEventListener("click", () => {
+    let data = btn.getAttribute("data-active");
+
+    for (const table of tables) {
+      if (data === table.getAttribute("data-tab").valueOf()) {
+        table.classList.remove("none");
+        console.log(table);
+      } else if (!table.classList.contains("none")) {
+        table.classList.add("none")
+      }
+    }
+    console.log(data);
+  });
+}
