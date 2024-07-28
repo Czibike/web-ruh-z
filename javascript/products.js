da2799d3e5d4ce070e39a7659911c982e71a05ed
Czibike <czibike03@gmail.com>
2024-07-28 13:37:38 +0200

    added pictures to kozmetika.html, made flipping treatments card and styling

--- javascript/products.js
+++ javascript/products.js
@@ -5,7 +5,7 @@ const products = {
       id: "",
       price: 123,
       image: "./images/acryl gel.webp",
-      desciption:
+      description:
         "Forradalmi újdonság a szakmában! A porcelán és az épít? Zselé alapanyagok egyesüléséből jött létre a FUSION ACRYL GEL. Könnyen, egyszerűen formázható, rugalmas mint a zselé és tartós mint a porcelán. SZAGTALAN, liquid helyett fixáló folyadékot és természetes szőrű ecsetet használjunk! UV, UV/LED lámpába köt, gyors munkát eredményez.",
     },
     {
@@ -13,7 +13,7 @@ const products = {
       id: "",
       price: 123,
       image: "./images/gyereksampon.webp",
-      desciption:
+      description:
         "Kifejezetten gyerekek mindennapos hajmosásához kifejlesztett hajsamponja, amely a teafaolajnak és cseresznye kivonatnak köszönhetően kíméletesen tisztít, könnyen kifésülhetővé és hidratáltá, egészségesebbé teszi a hajat. Használat: Vigye fel vizes hajra, masszírozza be, majd mossa le.",
     },
     {
@@ -21,7 +21,7 @@ const products = {
       id: "",
       price: 123,
       image: "./images/formázó paszta.webp",
-      desciption:
+      description:
         "Mattító paszta kaolinnal, valamint organikus quinoa- és orchidea-kivonattal dúsítva. Természetes felületet hoz létre, közepes tartást adva a kívánt formának. Könnyen szárad, és nem nehezíti le a hajat, vízzel könnyen lemosható. Tulajdonságok: 2-es fokozatú tartás: alacsony szintû rögzítés Kiszerelés: 100 ml",
     },
     {
@@ -29,7 +29,7 @@ const products = {
       id: "",
       price: 123,
       image: "./images/kondicionáló.webp",
-      desciption:
+      description:
         "Öblítést nem igénylő, kétfázisú vegán  kondicionáló, a szőke, szőkített és az ősz haj számára kifejlesztve. Argánolajban és Aloe Verában gazdag speciális formulája táplálja és fényessé teszi a hajat, miközben rugalmassá és lággyá teszi, valamint eltünteti a sárgás árnyalatokat. Használat: Masszírozza be a hajba egyenletesen, nem kell leöblíteni.",
     },
     {
@@ -37,7 +37,7 @@ const products = {
       id: "",
       price: 123,
       image: "./images/hajbalzsam.jpg",
-      desciption:
+      description:
         "Hajbalzsam szálkásodásra hajlamos hajhoz Olajsavval és marula olajjal Mélyreható ápolást nyújt Rugalmas és fényes haj Kevesebb töredezett hajvég",
     },
     {
@@ -45,7 +45,7 @@ const products = {
       id: "",
       price: 123,
       image: "./images/hajfesték.jpg",
-      desciption:
+      description:
         "Magas színintenzitás és erős haj. Érjen el egy intenzív és ragyogó színt, amely tökéletes az Ön számára. Hosszan tartó magas színintenzitás, ragyogás és ápolás a hajnak. Próbálja ki az ÚJ Palette Intensive Color Creme-t Ultimate Keratinnal. Kedvenc színe most hosszantartó, rendkívül intenzív színeredményt és ragyogó fényt ad. Fedezze fel a Palette Intensive Color Creme-t Ultimate Keratinnal a következőkért: hosszan tartó intenzitás, maximális színfény, egészséges és erős hajápolás, akár 100%-os szürke fedés és védelem a színkimosódás ellen.",
     },
     {
@@ -53,7 +53,7 @@ const products = {
       id: "",
       price: 123,
       image: "./images/hajlakk.jpg",
-      desciption:
+      description:
         "Hosszantartó frizurára vágyik amely bármilyen időjárásban tökéletes marad?  Akkor a Schwarzkopf Taft Ultra hajlakk erős fixálással ideális választás.  Védi a frizurát az erős széltől, magas páratartalommal, és az UV sugárzással szemben is.  Akár három napig fixálja frizuráját, anélkül hogy összetapasztaná a tincseket. Tulajdonságai: megbízhatóan rögzíti a frizurát akár 3 napon át rögzíti a frizurát anélkül hogy elnehezítené segít megvédeni a hajat a kiszáradástól egyszerűen, gyorsan felvihető a hajra nem hagy nyomokat a hajban Összetétele: arginin – erősíti a hajat a gyökerektől kezdve, védi a külső hatásoktól és hosszantartó rögzítést biztosít",
     },
     {
@@ -61,7 +61,7 @@ const products = {
       id: "",
       price: 123,
       image: "./images/hajkrém.jpg",
-      desciption:
+      description:
         "Hajformázó krém Hullámos, göndör hajra Hidratál és kondicionál Kókusz- és argánolajjal, E-vitaminnal Szilikonmentes A HASK Curl hajformázó krém gyengéden formázza hullámos, göndör fürtjeit.  A formula kókusz- és argánolajat és E-vitamint tartalmaz, ennek köszönhetően hidratálja és kondicionálja a hajat.  Szilikonmentes formulája fényessé és egészségessé teszi a hajat, megvédve azt a párától és a szöszösödéstől.",
     },
     {
@@ -69,7 +69,7 @@ const products = {
       id: "",
       price: 123,
       image: "./images/sampon.jpg",
-      desciption:
+      description:
         "A Silky Way Sampon természetes összetevőinek köszönhetően kíméletesen tisztítja meg a hajat a gyökerektől a végéig. Eltávolítja a fejbőrre lerakódott méreganyagokat, hogy hajad maximálisan friss és ápolt legyen.",
     },
   ],
@@ -79,7 +79,7 @@ const products = {
       id: "",
       price: 123,
       image: "./images/körömreszelő.webp",
-      desciption:
+      description:
         "100/180-as szemcseszerkezetû buffer reszelő. melynek használata elengedhetetlen a Gel lac technikánál és mûkörömépítésnél.",
     },
     {
@@ -87,7 +87,7 @@ const products = {
       id: "",
       price: 123,
       image: "./images/manikűrké.jpg",
-      desciption:
+      description:
         "Manikűr pedikűr készlet fekete. MAGAS TERMÉKMINŐSÉG! A körmös kiegészítők a legtartósabb anyagból - volfrámacélból készülnek . Minden acéltípus között a legjobb tulajdonságokkal rendelkezik. Magas perzisztencia jellemzi, ellenáll a kopásnak és a tompulásnak. Nem karcolódik és nem deformálódik. A készlet 16 elemből áll, az egyes szerszámok elegáns és tartós kialakítása a tokkal együtt hosszú használatot tesz lehetővé. Remek ajándékötlet is.  A tokban minden eszköznek külön helye van, amely lehetővé teszi a rend fenntartását, anélkül, hogy elveszítené azt. ",
     },
     {
@@ -95,7 +95,7 @@ const products = {
       id: "",
       price: 123,
       image: "./images/nivea-kezkrem-intensive-care.webp",
-      desciption:
+      description:
         "Kézápoló krém Mandulaolajat tartalma Eredeti NIVEA illattal Ápolja és intenzíven táplálja a bőrt A NIVEA® Intensive Care Kézápoló Krém mandulaolajat tartalmazó formulája az eredeti NIVEA illattal gazdagon ápolja és intenzíven táplálja a bőrt 24 órán át. Ez az ápoló krém a száraz bőrt is bársonyosan puhává varázsolja.",
     },
     {
@@ -104,7 +104,7 @@ const products = {
       price: 123,
       image:
         "./images/helia-d-sarokpuhito-krem-jojoba-olajjal-es-shea-vajjal.webp",
-      desciption:
+      description:
         "Az igénybevett és berepedezett bőr mindennapos ápolására szolgál. Jojoba-olajat és búzacsíra-kivonatot tartalmaz, amelyek segítik az érdes és nagyfokú fizikai igénybevételnek kitett bőrfelületek puhítását. Intenzíven hidratál, valamint hűsítő hatású.",
     },
   ],
@@ -114,7 +114,7 @@ const products = {
       id: "",
       price: 123,
       image: "./images/púder.jpg",
-      desciption:
+      description:
         "A Maybelline első mattító púdere, mely finomítja a pórusokat és akár 16 órán keresztül matt arcbőrt biztosít.",
     },
     {
@@ -122,7 +122,7 @@ const products = {
       id: "",
       price: 123,
       image: "./images/szempillla.jpg",
-      desciption:
+      description:
         " D íveltségű 0.12mm vastagságú 10mm hosszúságú selyem szempilla 16 sorban Prémium minőségű szempillák, melyek véghatása selyemfényű.  Könnyített súlya miatt Duplázó, illetve Klasszik (1D) technikához ajánlott. Egy tálcán belül egy méretezés.",
     },
     {
@@ -130,7 +130,7 @@ const products = {
       id: "",
       price: 123,
       image: "./images/szempillaspirál.jpg",
-      desciption:
+      description:
         " Innovatív Smart Fibre komplexszel, mely hármas hatást nyújt. JELLEMZŐI: Ultra dúsítás: különböző módon formázott szálak körülölelik a pillákat, így akár egész napon át gondoskodnak a volumenről és a hosszúságról. Ultra sokszorosítás: a formula kitölti a pillák közti réseket, így olyan hatást kelt, mintha több szempillád lenne. Ultra sűrűség: akár 100%-kal segít növelni a szempillák optikai sűrűségét, mandula formájú keféjének köszönhetően pedig egyenletesen oszlatja el a festéket a szempillákon, egészen a tövektől. Nem kenődik, nem pereg le.",
     },
     {
@@ -138,7 +138,7 @@ const products = {
       id: "",
       price: 123,
       image: "./images/szemtus.jpg",
-      desciption:
+      description:
         " Vízálló szemhéjtus Árnyalat: intenzív fekete matt Szuper vékony ecsetvég Akár 23 órán át fent marad* Kontaktlencsét viselőknek is alkalmas Az intenzív fekete matt trend !t up Calligraphy ultra precíz szemhéjtus szuper vékony ecsetvége lélegzetelállítóan precíz vonalvezetést tesz lehetővé. Vízálló formulája nem kenődik el és akár 23 órán át fent marad*.  Szemészetileg tesztelt, kontaktlencsét viselők számára is alkalmas.  *22 alannyal végzett belső teszt igazolja",
     },
     {
@@ -146,7 +146,7 @@ const products = {
       id: "",
       price: 123,
       image: "./images/szemceruza.jpg",
-      desciption:
+      description:
         "A vegán, mélyfekete alverde szemceruzával hangsúlyosan kiemelheti szemét, a puha kontúrceruzával könnyedén húzhat pontos, precíz vonalat, amely nem kenődik el. Nr. 1 - Fekete nem kenődik el puha hegyű ellenőrzött ökológiai gazdálkodásból származó mandulaolajjal és karnaubaviasszal minősített natúrkozmetikum Szemészetileg tesztelt, kontaktlencsét viselők is használhatják.",
     },
     {
@@ -154,7 +154,7 @@ const products = {
       id: "",
       price: 123,
       image: "./images/szemöldökceruza.png",
-      desciption:
+      description:
         "A szemöldök precíz modellezésére megfelelően kemény felrajzoló ceruza, mely ceruza alkalmazása a gyakorló bőrön is megfelelőnek bizonyult. A ceruza sűrű hegyezésével illetve a hegyező fertőtlenítésével elősegíthetjük a sminktetováló beavatkozásunk mikrobiológiai tisztaságát.",
     },
     {
@@ -162,7 +162,7 @@ const products = {
       id: "",
       price: 123,
       image: "./images/héjfesték.jpg",
-      desciption:
+      description:
         "Szeretné nap mint nap meglepni környezetét eredeti sminkjével? Az arcra és szemre való XX by Revolution SHADOW PALETTE fényűző többfunkciós sminkpaletta hihetetlen 66 színt kínál és velük végtelen sok sminkelési lehetőséget. Mindennap lehet valaki más, könnyen varázsolhat magának romantikus természetes megjelenést, de érzéki drámai hatásút is. Játsszon, alkosson, kísérletezzen, a matt és csillogó színek felragyogtatják arcát, bárhol is van. Tulajdonságai: 66 színárnyalatú szemfestéket tartalmaz matt és csillámló hatást is kínál bármilyen, kívánt megjelenés létrehozására alkalmas nappali és esti sminkhez is használható intenzív színű pigmentáció tartós fedés vegán összetétel",
     },
   ],
@@ -172,7 +172,7 @@ const products = {
       id: "",
       price: 123,
       image: "./images/borotva.webp",
-      desciption:
+      description:
         " Professzionális gép borotválkozáshoz, borotválkozáshoz, profilozáshoz vagy a szakáll, a nyak és a tarkó elhalványításához.  Sima és kényelmes borotválkozást végez, precízen vágja a szőrt.  Két acélpengéje természetesen siklik végig az arc és a fej körvonalain anélkül, hogy irritációt okozna.  Ergonomikus kialakítás, hogy kényelmesebb legyen a használat. Tulajdonságok: R.P.M: 6000 ford/perc. Felhasználási idő: 120 perc. Újratölthető lítium akkumulátor: 1400 mAh. Zaj: 70 dB. Teljesítmény: 5 W. Súly: 305 g. Adapter: Bemenet: 100/240 V. Kimenet: 5 VDC. Töltési idő: 2 óra. Frekvencia: 50/60 Hz. Tartalmazza az ecsetet és a zacskót. GÉP: 4,5 cm magas VÁGÁSFELÜLET: 2 cm széles",
     },
     {
@@ -180,7 +180,7 @@ const products = {
       id: "",
       price: 123,
       image: "./images/fésű.jpg",
-      desciption:
+      description:
         "Fedezze fel a praktikus és stílusos fekete fésű szettünket! Ez a szett tökéletes választás mindennapi hajápoláshoz és frizurakészítéshez. A különböző fésűk lehetővé teszik, hogy minden hajtípushoz és hajhosszhoz megtalálja a megfelelő fésűt. A szett négy különböző fésűből áll  A szett tartalma: 3 db hajvágó fésű: tupírozott frizurákhoz, kontyokhoz, hullámfésüléshez, hajformázáshoz, hajvágáshoz1 db stil fésű: műanyag nyelű stil fésű a vegyszeres munkák elvégzésére Szín:	Fekete Méret:  115 x 25 mm 167 x 25 mm 200 x 25 mm 205 x 35 mm Csomag tartalma: 4 db fésű Anyag:	Műanyag",
     },
     {
@@ -188,7 +188,7 @@ const products = {
       id: "",
       price: 123,
       image: "./images/hajcsatszett.jpg",
-      desciption:
+      description:
         "A szett 2 db barna és 4 db fekete, erős csiptetős csatot tartalmaz, melyek alkalmasak kisebb hajtincsek feltűzéséhez, kontyokhoz és egyéb frizurákhoz. Megfelelő erősséggel tartanak, több hajat is megbízhatóan fognak. Tökéletes választás a mindennapokra! Szín: barna, fekete Méret:  3 cm Anyaga: műanyag, fém Egészsegre káros anyagot nem tartalmaz.",
     },
     {
@@ -196,7 +196,7 @@ const products = {
       id: "",
       price: 123,
       image: "./images/hajgumi.jpg",
-      desciption:
+      description:
         "12 db rugalmas hajgumi egy henger alakú tárolódobozban. 6 féle szín egy dobozban. Minimum 35 mm átmérő, kb 4 mm vastagság. Rugalmas Kényelmes viselet 4 féle színösszeállítás (élénk, pasztell, fekete-fehér, barna) Méret:	35 x 4 mm Mennyiség:	12 db Szín:	Élénk: sárga, zöld narancssárga, kék, sötét rózsaszín, sötét lila Pasztell: sárga, zöld, világosbarna, kék, rózsaszín, lila Fekete - Fehér Világos-közép-sötétbarna",
     },
     {
@@ -204,7 +204,7 @@ const products = {
       id: "",
       price: 123,
       image: "./images/hajvágó.jpg",
-      desciption:
+      description:
         " Megbízható és felülmúlhatatlan vágási teljesítmény A Formula 1® Szakállvágó Rowenta TN384MF0 hosszantartó teljesítményt nyújt és egyetlen mozdulattal történő tökéletes vágást biztosít még sűrűbb szakáll esetén is. Kiválóan alkalmazkodik minden szakállhosszhoz. A 39 vágáshosszt lehetővé tevő pontos beállításnak köszönhetően bármely szakállstílust könnyedén megformázhat. A készülék titánbevonatú rozsdamentes acél pengéi optimális élességet biztosítanak minden nap, míg az akkumulátor 180 perc üzemidőt biztosító kapacitása és töltöttségi állapotának digitális monitorozása zavartalan és gond nélküli használatot biztosít. Műszaki adatok: Penge anyaga Rozsdamentes acél, Penge bevonata Titán, Motor fordulatszáma (f/p) 6500 ford./perc, Sebességfokozatok 1, Turbósebesség NEM, Vágási hossz kijelzése , Alkalmazási területek Szakáll, Szakállvágó pengefeltét IGEN, Szakállvágó pengefeltét mérete 39, Szakáll fésű típusa Állítható, Háromnapos borosta funkció IGEN, Szakállhossz-pozíciók 39, Vezeték nélküli működés 180 perc, Töltési idő 1h30, Gyorstöltés IGEN, Teljesítmény Vezeték nélküli és vezetékes használat, Akkumulátor típusa Lítium-ion, Levehető vágófej IGEN, Tisztítás Mosható fej, Tisztítókészlet Olaj + kefe, Töltésszint-jelző LCD, Tárolás Tárolótáska, Elektromos feszültség 100-240, Egyebek Fésűs toldóelem zárrendszere, Csatlakozóaljzat típusa Európai",
     },
     {
@@ -212,7 +212,7 @@ const products = {
       id: "",
       price: 123,
       image: "./images/hajszárító.jpg",
-      desciption:
+      description:
         "Kivételes szépségápolási termékek természetesen könnyű használattal: Fedezze fel a Rowenta Pure kollekcióját, hogy szépségápolási rutinját magasabb szintre emelje egy könnyen használható termékekkel, és a lehető legjobb formában léphessen ki az otthonából. A Rowenta Motion Dry hajszárító kivételesen gyorsan szárítja a hajat, és számos fejlett funkciót tartalmaz, többek között hat könnyen használható sebesség/hőmérséklet beállítást a hajformázás vezérléséhez, emellett két hajformázó tartozékot és további funkciókat tartalmaz a mindennapi könnyed hajformázáshoz.",
     },
   ],
