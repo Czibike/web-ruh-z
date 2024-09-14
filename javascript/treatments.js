ac9baf7817df65c6dd8e43cded0c7b6a60e333dc
Czibike <czibike03@gmail.com>
2024-09-14 18:01:39 +0200

    rewite kozmetika idopont1 with DRY code style

---
+++ javascript/treatments.js
@@ -0,0 +1,451 @@
+const treatments = {
+  "Image skincare Professzionális kezelések": [
+    {
+      headerTitle: "I PEEL hámlastó kezelések",
+      timeRequired: "30p",
+      cardTitle: "",
+    },
+    {
+      headerTitle: "IMAGE Exkluzív arc, nyak, - dekoltázs kezelések",
+      timeRequired: "30p",
+      cardTitle: "",
+    },
+  ],
+  "Mezoterápiás kezeles": [
+    {
+      headerTitle: "Szemkörnyékre",
+      timeRequired: "30p",
+      cardTitle: "",
+    },
+    {
+      headerTitle: "Arc",
+      timeRequired: "30p",
+      cardTitle: "",
+    },
+    {
+      headerTitle: "Arc + nyak",
+      timeRequired: "30p",
+      cardTitle: "",
+    },
+    {
+      headerTitle: "Arc + nyak + dekoltázs",
+      timeRequired: "30p",
+      cardTitle: "",
+    },
+    {
+      headerTitle: "Innovera Biorevitalizációs ampullával",
+      timeRequired: "30p",
+      cardTitle: "",
+    },
+    {
+      headerTitle: "Hajkezelés (hajhullás, regenerálás)",
+      timeRequired: "30p",
+      cardTitle: "",
+    },
+  ],
+  "Rádiófrekvencia - Hatékony ránckezelé, feszesítés": [
+    {
+      headerTitle: "Arc keztelés",
+      timeRequired: "30p",
+      cardTitle: "1 alkalom",
+    },
+    {
+      headerTitle: "Arc keztelés",
+      timeRequired: "30p",
+      cardTitle: "6-os bérlet + 1",
+    },
+    {
+      headerTitle: "Toka kezelés",
+      timeRequired: "30p",
+      cardTitle: "1 alkalom",
+    },
+    {
+      headerTitle: "Toka kezelés",
+      timeRequired: "30p",
+      cardTitle: "6-os bérlet + 1",
+    },
+  ],
+  "Mezoterápiás kezelések, kollagén indukcióval": [
+    {
+      headerTitle: "Arc + nyak",
+      timeRequired: "30p",
+      cardTitle: "DermaPen mezoterápiás hatóanyag bevitel, kollagén indukció",
+    },
+    {
+      headerTitle: "Arc + nyak + dekoltázs",
+      timeRequired: "30p",
+      cardTitle: "DermaPen mezoterápiás hatóanyag bevitel, kollagén indukció",
+    },
+  ],
+  "alakformáló feszítő kezelések": [
+    {
+      headerTitle: "Kar kezelés",
+      timeRequired: "30p",
+      cardTitle: "1 alkalom",
+    },
+    {
+      headerTitle: "Comb kezelés",
+      timeRequired: "30p",
+      cardTitle: "1 alkalom",
+    },
+    {
+      headerTitle: "Has kezelés",
+      timeRequired: "30p",
+      cardTitle: "1 alkalom",
+    },
+    {
+      headerTitle: "Kar kezelés",
+      timeRequired: "30p",
+      cardTitle: "6-os bérlet + 1",
+    },
+    {
+      headerTitle: "Comb kezelés",
+      timeRequired: "30p",
+      cardTitle: "6-os bérlet + 1",
+    },
+    {
+      headerTitle: "Has kezelés",
+      timeRequired: "30p",
+      cardTitle: "6-os bérlet + 1",
+    },
+  ],
+  "Cellulit feszítő kezelések": [
+    {
+      headerTitle: "1 alkalom",
+      timeRequired: "30p",
+      cardTitle: "",
+    },
+    {
+      headerTitle: "4-es bérlet",
+      timeRequired: "30p",
+      cardTitle: "",
+    },
+    {
+      headerTitle: "6-os bérlet",
+      timeRequired: "30p",
+      cardTitle: "",
+    },
+  ],
+  "Krio fagyasztós zsirbontás": [
+    {
+      headerTitle: "1 terület",
+      timeRequired: "30p",
+      cardTitle: "",
+    },
+    {
+      headerTitle: "2 terület",
+      timeRequired: "1ó",
+      cardTitle: "",
+    },
+    {
+      headerTitle: "3 terület",
+      timeRequired: "1ó 30p",
+      cardTitle: "",
+    },
+    {
+      headerTitle: "4 terület",
+      timeRequired: "2ó",
+      cardTitle: "",
+    },
+  ],
+  infraszauna: [
+    {
+      headerTitle: "Infaszauna",
+      timeRequired: "30p",
+      cardTitle:
+        "Maradj egészséges, miközben fogysz-feszesedsz is! (1 alkalom)",
+    },
+    {
+      headerTitle: "Infaszauna",
+      timeRequired: "30p",
+      cardTitle:
+        "Maradj egészséges, miközben fogysz-feszesedsz is! (5-ös bérlet)",
+    },
+  ],
+  "Expressz arckezelések": [
+    {
+      headerTitle: "Letisztitás, Ultrahang vagy intoforézis, zárókrém",
+      timeRequired: "30p",
+      cardTitle: "",
+    },
+    {
+      headerTitle: "Letisztitás, Ultrahang vagy intoforézis, zárókrém",
+      timeRequired: "30p",
+      cardTitle: "+ Arc masszázzsal",
+    },
+    {
+      headerTitle: "Letisztitás, Ultrahang vagy intoforézis, zárókrém",
+      timeRequired: "30p",
+      cardTitle: "+ Arc+dekoltázs masszázzsal",
+    },
+    {
+      headerTitle: "Hidrabrázió + Ampulla/ml",
+      timeRequired: "30p",
+      cardTitle:
+        "letisztítás, peeling, gőz, manuális tisztítás probléma megoldó pakolás, zárókrém",
+    },
+  ],
+  Arckezelések: [
+    {
+      headerTitle: "Arckezelés",
+      timeRequired: "30p",
+      cardTitle:
+        "letisztítás, peeling, gőz, manuális tisztítás probléma megoldó pakolás, zárókrém",
+    },
+    {
+      headerTitle: "Arckezelés arcmasszázzsal",
+      timeRequired: "30p",
+      cardTitle:
+        "letisztítás, peeling, gőz, manuális tisztítás, pakolás, arcmasszázs, +krém",
+    },
+    {
+      headerTitle: "Klasszikus arckezelés arc-nyak-dekoltázsmasszázs",
+      timeRequired: "30p",
+      cardTitle:
+        "letisztítás, peeling, gőz, manuális tisztítás, arc-nyak-dekoltázsmasszázs, pakolás, befejező krém",
+    },
+  ],
+  "Kozmetikai masszázsok": [
+    {
+      headerTitle: "Arcmasszázs",
+      timeRequired: "30p",
+      cardTitle:
+        "Személyre, problémára szabott kezelést kínálunk. A stresszoldás, nyugalom, feszültség elengedése a kezelés fontos szempontja.",
+    },
+    {
+      headerTitle: "Arc-Nyak-Dekolázs masszázs",
+      timeRequired: "45p",
+      cardTitle:
+        "Személyre, problémára szabott kezelést kínálunk. A stresszoldás, nyugalom, feszültség elengedése a kezelés fontos szempontja.",
+    },
+  ],
+  "Szempilla-, szemöldök festés, ápolás": [
+    {
+      headerTitle: "Szempilla festés",
+      timeRequired: "30p",
+      cardTitle:
+        "A szem a lélek tükre. Emeld ki vonzó szemöldökökkel és szempillákkal!",
+    },
+    {
+      headerTitle: "Szemöldök festés",
+      timeRequired: "30p",
+      cardTitle:
+        "A szem a lélek tükre. Emeld ki vonzó szemöldökökkel és szempillákkal!",
+    },
+    {
+      headerTitle: "Szempilla igazítás + utókezelés",
+      timeRequired: "30p",
+      cardTitle:
+        "A szem a lélek tükre. Emeld ki vonzó szemöldökökkel és szempillákkal!",
+    },
+    {
+      headerTitle: "Szemöldök szedés, festés + utókezelés",
+      timeRequired: "30p",
+      cardTitle:
+        "A szem a lélek tükre. Emeld ki vonzó szemöldökökkel és szempillákkal!",
+    },
+  ],
+  Smink: [
+    {
+      headerTitle: "Nappali smink",
+      timeRequired: "30p",
+      cardTitle:
+        "Ha nincs időd otthon megcsinálni vagy nem tetszik a saját sminkelési technikád, gyere be hozzánk és mi megcsináljuk neked a tökéletes sminkedet.",
+    },
+    {
+      headerTitle: "Alkalmi smink",
+      timeRequired: "30p",
+      cardTitle: "Microblading, szálazott technikával extra természetes hatás",
+    },
+    {
+      headerTitle: "Esküvői smink (próba sminkkel)",
+      timeRequired: "30p",
+      cardTitle:
+        "Tökéletes sminket akarsz a &quot;nagy napra&quot;? Mi ezt is el tudjuk neked készíteni. Gyere el előtte és megvalósítjuk a terveidet, hogy ne pont előtte menjen az igazításokkal az idő.",
+    },
+  ],
+  "Szemöldök tetoválás": [
+    {
+      headerTitle: "Szemöldök tetoválás",
+      timeRequired: "30p",
+      cardTitle: "Microblading, szálazott technikával extra természetes hatás",
+    },
+  ],
+  "Lézeres szőrtelenítés > Nők": [
+    {
+      headerTitle: "Mikro terület sörtelenítése",
+      timeRequired: "30p",
+      cardTitle:
+        "Bajusz, áll, pajesz, hónalj, lábfej, térd, kézfej és ujjak, mellbimbó körül, hascsík, köldökcsík vagy mellek közötti csík végleges szőrtelenítése",
+    },
+    {
+      headerTitle: "Kis terület sörtelenítése",
+      timeRequired: "30p",
+      cardTitle:
+        "Hónalj, orca, nyak, bikini, bikinivonal végleges szőrtelenítése",
+    },
+    {
+      headerTitle: "Közepes terület sörtelenítése",
+      timeRequired: "30p",
+      cardTitle:
+        "Alkar, felkar, mellkas, has, fenék vagy váll végleges szőrtelenítése",
+    },
+    {
+      headerTitle: "Nagy terület sörtelenítése",
+      timeRequired: "30p",
+      cardTitle:
+        "Teljes arc, teljes intim, derék, lábszár, comb vagy hát végleges szőrtelenítése",
+    },
+    {
+      headerTitle: "Több terület sörtelenítése",
+      timeRequired: "30p",
+      cardTitle:
+        "25% egyidejű kedvezmény! Több terület egyidejű kezelésénél minden 3. terület után egy terület ajándék!",
+    },
+    {
+      headerTitle: "Teljes test sörtelenítése",
+      timeRequired: "30p",
+      cardTitle: "Akárhány terület egyidejű kezelése",
+    },
+  ],
+  "Lézeres szőrtelenítés > Férfiak": [
+    {
+      headerTitle: "Mikro terület sörtelenítése",
+      timeRequired: "30p",
+      cardTitle:
+        "Bajusz, áll, pajesz, hónalj, lábfej, térd, kézfej és ujjak, mellbimbó körül, hascsík, köldökcsík vagy mellek közötti csík végleges szőrtelenítése",
+    },
+    {
+      headerTitle: "Kis terület sörtelenítése",
+      timeRequired: "30p",
+      cardTitle:
+        "Hónalj, orca, nyak, bikini, bikinivonal végleges szőrtelenítése",
+    },
+    {
+      headerTitle: "Közepes terület sörtelenítése",
+      timeRequired: "30p",
+      cardTitle:
+        "Alkar, felkar, mellkas, has, fenék vagy váll végleges szőrtelenítése",
+    },
+    {
+      headerTitle: "Nagy terület sörtelenítése",
+      timeRequired: "30p",
+      cardTitle:
+        "Teljes arc, teljes intim, derék, lábszár, comb vagy hát végleges szőrtelenítése",
+    },
+    {
+      headerTitle: "Több terület sörtelenítése",
+      timeRequired: "30p",
+      cardTitle:
+        "25% egyidejű kedvezmény! Több terület egyidejű kezelésénél minden 3. terület után egy terület ajándék!",
+    },
+    {
+      headerTitle: "Teljes test sörtelenítése",
+      timeRequired: "30p",
+      cardTitle: "Akárhány terület egyidejű kezelése",
+    },
+  ],
+  "Szőrtelenítés > Nők": [
+    {
+      headerTitle: "Mikro terület sörtelenítése",
+      timeRequired: "30p",
+      cardTitle:
+        "Bajusz, áll, pajesz, hónalj, lábfej, térd, kézfej és ujjak, mellbimbó körül, hascsík, köldökcsík vagy mellek közötti csík szőrtelenítése",
+    },
+    {
+      headerTitle: "Kis terület sörtelenítése",
+      timeRequired: "30p",
+      cardTitle: "Hónalj, orca, nyak, bikini, bikinivonal szőrtelenítése",
+    },
+    {
+      headerTitle: "Közepes terület sörtelenítése",
+      timeRequired: "30p",
+      cardTitle: "Alkar, felkar, mellkas, has, fenék vagy váll szőrtelenítése",
+    },
+    {
+      headerTitle: "Nagy terület sörtelenítése",
+      timeRequired: "30p",
+      cardTitle:
+        "Teljes arc, teljes intim, derék, lábszár, comb vagy hát szőrtelenítése",
+    },
+    {
+      headerTitle: "Több terület sörtelenítése",
+      timeRequired: "30p",
+      cardTitle:
+        "25% egyidejű kedvezmény! Több terület egyidejű kezelésénél minden 3. terület után egy terület ajándék!",
+    },
+    {
+      headerTitle: "Teljes test sörtelenítése",
+      timeRequired: "30p",
+      cardTitle: "Akárhány terület egyidejű kezelése",
+    },
+  ],
+  "Szőrtelenítés > Férfiak": [
+    {
+      headerTitle: "Mikro terület sörtelenítése",
+      timeRequired: "30p",
+      cardTitle:
+        "Bajusz, áll, pajesz, hónalj, lábfej, térd, kézfej és ujjak, mellbimbó körül, hascsík, köldökcsík vagy mellek közötti csík szőrtelenítése",
+    },
+    {
+      headerTitle: "Kis terület sörtelenítése",
+      timeRequired: "30p",
+      cardTitle: "Hónalj, orca, nyak, bikini, bikinivonal szőrtelenítése",
+    },
+    {
+      headerTitle: "Közepes terület sörtelenítése",
+      timeRequired: "30p",
+      cardTitle: "Alkar, felkar, mellkas, has, fenék vagy váll szőrtelenítése",
+    },
+    {
+      headerTitle: "Nagy terület sörtelenítése",
+      timeRequired: "30p",
+      cardTitle:
+        "Teljes arc, teljes intim, derék, lábszár, comb vagy hát szőrtelenítése",
+    },
+    {
+      headerTitle: "Több terület sörtelenítése",
+      timeRequired: "30p",
+      cardTitle:
+        "25% egyidejű kedvezmény! Több terület egyidejű kezelésénél minden 3. terület után egy terület ajándék!",
+    },
+    {
+      headerTitle: "Teljes test sörtelenítése",
+      timeRequired: "30p",
+      cardTitle: "Akárhány terület egyidejű kezelése",
+    },
+  ],
+};
+const container = document.getElementById("container")
+
+let data = "";
+
+for (const index in treatments) {
+  data += `<details>
+  <summary>${index}</summary>`;
+
+  const arr = treatments[index];
+  arr.forEach((treatment) => {
+    data += `
+    <div class="card">
+      <div class="card-header">
+        <div class="img">
+          <img
+            src="./images/no-photo.png"
+            alt="no photo"
+            width="50"
+            height="50"
+          />
+        </div>
+        <div class="card-header-text">
+          <div class="header-title">${treatment.headerTitle}</div>
+          <span class="time-required">${treatment.timeRequired}</span>
+        </div>
+      </div>
+      <div class="card-title">
+        <span>${treatment.cardTitle}</span>
+      </div>
+    </div>`;
+  });
+  data += `</details>`
+}
