ef0662b37a86cbfae9432bd0b0820a137b22c92b
Czibike <czibike03@gmail.com>
2024-10-07 21:38:29 +0200

    made product generator

--- javascript/cart/cart.js
+++ javascript/cart/cart.js
@@ -1,7 +1,17 @@
-let cart = [1];
+let cart = [
+  {
+    imgSrc: "./images/borotva.webp",
+    name: "borotva",
+    quantity: 1,
+    priceN: 2000,
+    priceT: 4000,
+  },
+];
 
 const empty = document.querySelector(".cart-group-empty");
 const emptyNot = document.querySelector(".cart-group");
+const itemContainer = document.querySelector(".item-wrapper");
+let itemTemplete = "";
 
 if (cart.length === 0) {
   empty.classList.remove("none");
@@ -10,3 +20,49 @@ if (cart.length === 0) {
   empty.classList.add("none");
   emptyNot.classList.remove("none");
 }
+
+cart.forEach((item) => {
+  itemTemplete += `
+    <div class="item">
+      <div class="item-img center">
+        <img src="${item.imgSrc}" alt="cart-item" />
+      </div>
+      <div class="item-name center">
+        <a href="#" class="js-link">
+          <h3>${item.name}</h3>
+        </a>
+        <br>
+        <span>Kapott hűségpontok: 10</span>
+      </div>
+      <div class="quantity">
+        <div class="stepper">
+          <input type="number" value="${item.quantity}" />
+          <div class="stepper-btns">
+            <button type="button">
+              <span class="material-symbols-outlined">
+                keyboard_arrow_up
+              </span>
+            </button>
+            <button type="button">
+              <span class="material-symbols-outlined">
+                keyboard_arrow_down
+              </span>
+            </button>
+          </div>
+        </div>
+        <div class="delete-btn">
+          <button type="button">
+            <span class="material-symbols-outlined"> close </span>
+          </button>
+        </div>
+      </div>
+      <div class="unit-price center">
+        <span class="js-unit-price">${item.priceN} Ft</span>
+      </div>
+      <div class="total-price center">
+        <span class="js-total-price">${item.priceT} Ft</span>
+      </div>
+    </div>
+  `;
+  itemContainer.innerHTML = itemTemplete
+});
